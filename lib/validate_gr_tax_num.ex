e88fe09749a0a9948f748cf7330e00fb22c43794
Serafeim Papastefanos <spapas@gmail.com>
2019-05-03 23:08:37 +0300

    Try to improve code

--- lib/validate_gr_tax_num.ex
+++ lib/validate_gr_tax_num.ex
@@ -6,7 +6,6 @@ defmodule ValidateGrTaxNum do
   them is a checksum digit and the others are used
   to compute that checksum through a rather simple
   algorithm.
-
   """
 
   @doc """
@@ -22,13 +21,19 @@ defmodule ValidateGrTaxNum do
       iex> ValidateGrTaxNum.valid("000000001")
       false
 
+      iex> ValidateGrTaxNum.valid("123123123")
+      false
+
       iex> ValidateGrTaxNum.valid("094277965")
       true
 
-      iex> ValidateGrTaxNum.valid("094 27796 5")
+      iex> ValidateGrTaxNum.valid("0 94 27796 5")
       true
 
-      iex> ValidateGrTaxNum.valid("094-27796-5")
+      iex> ValidateGrTaxNum.valid("0-14 27796 5")
+      false
+
+      iex> ValidateGrTaxNum.valid("094-277-96-5")
       true
 
       iex> ValidateGrTaxNum.valid("09427796")
@@ -39,42 +44,50 @@ defmodule ValidateGrTaxNum do
 
   """
   def valid(val) do
+    case val |> covert_to_digits |> get_digits_and_check do
+      {check, digits} ->
+        # Sum the digits and multiply them by their position-corresponding 2-based power
+        # and get their rem with 11
+        check ==
+          digits
+          |> Enum.with_index()
+          |> Enum.map(fn {d, idx} -> (d * :math.pow(2, idx + 1)) |> round end)
+          |> Enum.sum()
+          |> rem(11)
+          # Check if the previous calc is equal to the check digit; If the rem was 10
+          # then check if the check digit is 0.
+          |> case do
+            10 -> 0
+            v -> v
+          end
+
+      _ ->
+        false
+    end
+  end
+
+  defp covert_to_digits(val) do
     # Convert the digits to integers. If there are forbitten characters
     # return an empty array
-    all_digits =
-      try do
-        val
-        |> String.replace(~r/[ -]/, "")
-        |> String.graphemes()
-        |> Enum.map(&String.to_integer/1)
-      rescue
-        ArgumentError -> []
-      end
+    try do
+      val
+      |> String.replace(~r/[ -]/, "")
+      |> String.graphemes()
+      |> Enum.map(&String.to_integer/1)
+    rescue
+      ArgumentError -> []
+    end
+  end
 
+  defp get_digits_and_check(all_digits) do
     # If basic checks don't pass then return false
     if length(all_digits) != 9 || all_digits |> Enum.sum() == 0 do
       false
     else
-      # Retrieve the check digit and reverse the rest of the digits for easier manipulation
-      {check, digits} =
-        all_digits
-        |> Enum.reverse()
-        |> List.pop_at(0)
-
-      # Sum the digits and multiply them by their position-corresponding 2-based power
-      # and get their rem with 11
-      check ==
-        digits
-        |> Enum.with_index()
-        |> Enum.map(fn {d, idx} -> (d * :math.pow(2, idx + 1)) |> round end)
-        |> Enum.sum()
-        |> rem(11)
-        # Check if the previous calc is equal to the check digit; If the rem was 10
-        # then check if the check digit is 0.
-        |> case do
-          10 -> 0
-          v -> v
-        end
+      # Return the check digit and reverse the rest of the digits for easier manipulation
+      all_digits
+      |> Enum.reverse()
+      |> List.pop_at(0)
     end
   end
 end
