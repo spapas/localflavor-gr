27c04aee78d0f7a79eccf6262685ff6475f25ff6
Serafeim Papastefanos <spapas@gmail.com>
2019-07-18 10:09:06 +0300

    Validate amka is ok

--- lib/validate_amka.ex
+++ lib/validate_amka.ex
@@ -14,66 +14,97 @@ defmodule ValidateAMKA do
 
   ## Examples
 
-      iex> ValidateAMKA.valid("00000000000")
+      #iex> ValidateAMKA.valid("00000000000")
+      #false
+
+      #iex> ValidateAMKA.valid("000000001")
+      #false
+
+      #iex> ValidateAMKA.valid("123123123")
+      #false
+
+      #iex> ValidateAMKA.valid("993123123")
+      #false
+
+      iex> ValidateAMKA.valid("21068302673")
       false
 
-      iex> ValidateAMKA.valid("000000001")
-      false
-
-      iex> ValidateAMKA.valid("123123123")
-      false
-
-      iex> ValidateAMKA.valid("2458202672")
+      iex> ValidateAMKA.valid("21068302674")
       true
+
+      iex> ValidateAMKA.valid("20108201821")
+      true
+
+      iex> ValidateAMKA.valid("02058401528")
+      true
+
+      iex> ValidateAMKA.valid("52058401523")
+      false
+
 
   """
   def valid(val) do
-    case val |> covert_to_digits |> get_digits_and_check do
-      {check, digits} ->
-        # Sum the digits and multiply them by their position-corresponding 2-based power
-        # and get their rem with 11
-        check ==
-          digits
-          |> Enum.zip(@multipliers)
-          |> Enum.map(fn {x, y} -> x * y end)
-          |> Enum.sum()
-          |> rem(11)
-          # Check if the previous calc is equal to the check digit; If the rem was 10
-          # then check if the check digit is 0.
-          |> case do
-            10 -> 0
-            v -> v
-          end
-
-      _ ->
+    case val |> covert_to_digits do
+      :error ->
         false
+
+      digits ->
+        # 1. Reverse the digits and add an index to them
+        digits
+        |> Enum.reverse
+        |> Enum.with_index()  
+        # Then sum the digits either as they are or using the luhn_double_digit
+        |> Enum.map(fn {d, idx} -> if rem(idx, 2) == 0, do: d, else: luhn_double_digit(d) end)
+        |> Enum.sum()
+        # And finally check the mod 10 and return true if it is 0
+        |> rem(10)
+        |> case do
+          0 -> true
+          _ -> false
+        end
     end
   end
 
-  defp get_digits_and_check(all_digits) do
-    # If basic checks don't pass then return false
-    if length(all_digits) != 11 || Enum.sum(all_digits) == 0 do
-      false
-    else
-      # Return the check digit (last) and the rest of the digits
-      all_digits
-      |> List.pop_at(8)
-    end
+  defp luhn_double_digit(d) do
+    # This will return the 2*digit as it is if it is < 9 or it will the sum of the digits if it is >= 10
+    # i.e 18 = 1+8 = 9
+    d2 = d * 2
+    if d2 > 9, do: 1+(d2-10), else: d2
+  end
+
+  defp is_valid_date(<<d1, d2, m1, m2, y1, y2>>)
+       when d1 in ?0..?9 and d2 in ?0..?9 and
+              m1 in ?0..?9 and m2 in ?0..?9 and y1 in ?0..?9 and y2 in ?0..?9 do
+    # Converting the DDMMYY date thingie to 19YY-MM-DD to pase it to Date.from_iso8601 and check if it is valid:
+    # solution 1. Regex.replace(~r/(\d\d)(\d\d)(\d\d)/, "250611", "19\\3-\\2-\\1")
+    # solution 2. "250611" |> String.graphemes |> Enum.chunk_every(2) |> Enum.reverse |> Enum.flat_map(& &1) |> List.insert_at(2,"-") |> List.insert_at(5,"-") |> List.insert_at(0, "19") |> Enum.join |> Date.from_iso8601
+    # solution 3. <<day :: binary-size(2), month :: binary-size(2), year :: binary-size(2)>> = "250611"
+    # solution 4a. ( fn(<<d1, d2, m1, m2, y1, y2>>) -> <<?1, ?9, y1, y2, ?-, m1, m2, ?-, d1, d2>> end ).("230419")
+    # solution 4b. (same as with 4a but with guards):
+    # (fn(<<d1, d2, m1, m2, y1, y2>>) when  d1 in ?0..?9 and d2 in ?0..?9 and  m1 in ?0..?9 and m2 in ?0..?9 and y1 in ?0..?9 and y2 in ?0..?9 -> <<?1, ?9, y1, y2, ?-, m1, m2, ?-, d1, d2>> end ).("230413")
+    <<?1, ?9, y1, y2, ?-, m1, m2, ?-, d1, d2>>
+    |> Date.from_iso8601() 
+    
   end
 
   defp covert_to_digits(val) do
     # Convert the digits to integers. If there are forbitten characters
     # return an empty array
+    
+    okval = val |> String.replace(~r/[ -]/, "")
 
-    # fix date Regex.replace(~r/(\d\d)(\d\d)(\d\d)/, "250611", "19\\3-\\2-\\1")
-    # or "150615" |> String.graphemes |> Enum.chunk_every(2) |> Enum.reverse |> Enum.flat_map(& &1) |> List.insert_at(2,"-") |> List.insert_at(5,"-") |> List.insert_at(0, "19") |> Enum.join |> Date.from_iso8601
-    try do
-      val
-      |> String.replace(~r/[ -]/, "")
-      |> String.graphemes()
-      |> Enum.map(&String.to_integer/1)
-    rescue
-      ArgumentError -> []
+    case okval |> String.slice(0, 6) |> is_valid_date do
+      {:ok, _} ->
+        try do
+          okval
+          |> String.graphemes()
+          |> Enum.map(&String.to_integer/1)
+        rescue
+          ArgumentError -> :error
+        end
+
+      _ ->
+        :error
     end
   end
 end
