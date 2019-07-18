925b1d3a2b3cfd439f2a8075e9e1396f8a3c03ee
Serafeim Papastefanos <spapas@gmail.com>
2019-07-18 11:48:42 +0300

    Publish package

--- mix.exs
+++ mix.exs
@@ -6,20 +6,34 @@ defmodule LocalflavorGr.MixProject do
       app: :localflavor_gr,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      name: "LocalflavorGR",
+      source_url: "https://github.com/spapas/localflavor-gr"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  defp description() do
+    "A very simple project for validating 9-digit greek tax numbers and 11-digit greek social security numbers."
+  end
+
+  defp package() do
     [
-      extra_applications: [:logger]
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*  LICENSE* ),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/spapas/localflavor-gr"}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  def application do
     []
+  end
+
+  defp deps do
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
